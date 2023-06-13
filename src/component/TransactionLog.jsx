2f6e8f7ff37ed09d855580950783e7142128dbdb
sagar11sachin <124698334+sagar11sachin@users.noreply.github.com>
2023-06-13 17:26:31 +0530

    Update TransactionLog.jsx

--- src/component/TransactionLog.jsx
+++ src/component/TransactionLog.jsx
@@ -7,11 +7,30 @@ const TransactionLog = () => {
   const [isOpen, setIsOpen] = useState(false); // State variable to track whether the log is open or closed
 
   const handleToggleDropdown = () => {
+    // ***TODO***
      // Toggle the isOpen state value when the button is clicked
   };
 
   return (
     <div className="transaction-log-container">
+    <h2>Transaction Log</h2>
+      <div>
+        <button
+          className="transaction-log-button"
+          onClick={handleToggleDropdown}
+        >
+          {isOpen ? 'Hide Logs' : 'Show Logs'}
+        </button>
+        {isOpen && (
+          <ul className="transaction-log-list">
+            {transactions.map((transaction, index) => (
+              <li className="transaction-log-item" key={index}>
+                Action: {transaction.action}, Amount: {transaction.amount}, Total Balance: {transaction.balance}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
       
     </div>
   );
